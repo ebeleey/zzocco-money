b123e3beda511e1a6f8364320a468b9cc61d5803
hyvnsevng <yh189487@gmail.com>
2024-11-20 17:01:14 +0900

    feat: 로그인 페이지 및 토큰 인증 구현 (FE)

--- front/zzocco_money/src/views/LoginView.vue
+++ front/zzocco_money/src/views/LoginView.vue
@@ -1,53 +1,58 @@
 <template>
 	<div class="login-container">
-	  <h2>로그인</h2>
-	  <form @submit.prevent="handleLogin">
+		<h2>로그인</h2>
+		<form @submit.prevent="handleLogin">
 		<div class="form-group">
-		  <label for="username">아이디</label>
-		  <input type="text" id="username" v-model="username" placeholder="아이디를 입력하세요" required />
+			<label for="username">아이디</label>
+			<input type="text" id="username" v-model="username" placeholder="아이디를 입력하세요" required />
 		</div>
 		
 		<div class="form-group">
-		  <label for="password">비밀번호</label>
-		  <input type="password" id="password" v-model="password" placeholder="비밀번호를 입력하세요" required />
+			<label for="password">비밀번호</label>
+			<input type="password" id="password" v-model="password" placeholder="비밀번호를 입력하세요" required />
 		</div>
-  
+
 		<div class="form-group">
-		  <button type="submit" class="submit-button">로그인</button>
+			<button type="submit" class="submit-button">로그인</button>
 		</div>
-	  </form>
-  
-	  <p class="signup-link">
+		</form>
+
+		<p class="signup-link">
 		계정이 없으신가요? <RouterLink to="/signup">회원가입</RouterLink>
-	  </p>
+		</p>
 	</div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { RouterLink, useRouter } from 'vue-router'
-  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+import { useAccountStore } from '@/stores/account';
+
   const router = useRouter()
-  
+  const store = useAccountStore()
   // 이메일과 비밀번호를 바인딩
-  const username = ref('')
-  const password = ref('')
+  const username = ref(null)
+  const password = ref(null)
   
   // 로그인 처리 함수
   const handleLogin = () => {
+		store.logIn({
+			username: username.value,
+			password: password.value
+		})
 	// 실제 로그인 로직 (백엔드 API 호출 등) 필요
-	console.log('로그인 시도:', username.value, password.value)
+	// console.log('로그인 시도:', username.value, password.value)
 	
 	// 성공적으로 로그인한 후 메인 페이지로 리다이렉트
 	// 여기에 인증 로직을 추가해야 합니다.
-	router.push('/')
+	
   }
   </script>
   
   <style scoped>
   .login-container {
 	width: 300px;
-	margin: 0 auto;
+	margin: 100px auto 0;
 	padding: 20px;
 	border: 1px solid #ddd;
 	border-radius: 5px;
