031d3e221e4126f484cd708dd08e0caa97226763
이다이 <ebeleey@gmail.com>
2024-11-20 14:29:08 +0900

    feat: navbar+ background color 변경

--- front/zzocco_money/src/components/navbars/NavBar.vue
+++ front/zzocco_money/src/components/navbars/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark"> <!-- bg-dark로 배경을 검정색으로, navbar-dark로 글씨를 흰색으로 설정 -->
+    <nav class="navbar navbar-expand-md navbar-dark"> <!-- bg-dark로 배경을 검정색으로, navbar-dark로 글씨를 흰색으로 설정 -->
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div class="collapse navbar-collapse" id="navbarToggler">
           <!-- 로고는 왼쪽에 기본적으로 위치합니다. -->
           <RouterLink :to="{name: 'home'}" class="nav-link"><img src="../../assets/logo_wave.png" alt="logo"></RouterLink>
 
@@ -43,35 +43,16 @@
 </script>
 
 <style scoped>
-.nav-link {
-  color: white; /* 부모 요소의 색상 상속 */
-  text-decoration: none; /* 밑줄 제거 */
-  font-size: inherit; /* 폰트 크기 상속 */
-  cursor: pointer; /* 마우스 포인터 설정 */
-}
+
+
 /* Navbar */
 .navbar {
-  font-family: 'MabinogiClassicR';
   font-size: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  background-color: #3f2411; /* 갈색 */
-  border-bottom: 1px solid #6D3510; /* 어두운 갈색 테두리 */
-}
-.nav-links {
-  list-style: none;
-  display: flex;
-  gap: 15px;
-}
-.nav-links .nav-link {
-  text-decoration: none;
-  color: #fff; /* 글자 색상을 흰색으로 설정 */
-  font-size: inherit;
-}
-.nav-links .nav-link:hover {
-  color: #FFD700; /* 호버 시 글자 색상을 금색으로 변경 */
+
 }
 
 img {
