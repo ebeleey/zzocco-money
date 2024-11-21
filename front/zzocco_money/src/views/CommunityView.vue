c6a443e5ae5aa6102eb3feb75a05f6c355761567
ebeleey <ebeleey@gmail.com>
2024-11-21 23:10:54 +0900

    feat: 커뮤니티 탭 틀만 구현(FE)

--- front/zzocco_money/src/views/CommunityView.vue
+++ front/zzocco_money/src/views/CommunityView.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <h1>게시판</h1>
+        <h1 class="page-title">게시판</h1>
+        <!-- 탭 버튼 -->
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">전체</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">금융 꿀팁</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">인증게시판</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">자유게시판</a>
+            </li>
+        </ul>
+        <hr>
     </div>
 </template>
 
@@ -9,5 +25,21 @@
 </script>
 
 <style lang="scss" scoped>
+.page-title {
+  text-align: center;
+  margin-top: 70px; 
+  margin-bottom: 50px; 
+  font-weight: bold;
+}
 
+.nav-link {
+//   font-size:large;
+  color: black;
+}
+
+hr {
+  margin: 0 auto;
+  width: 50%;
+  color: #3f2411;
+}
 </style>
