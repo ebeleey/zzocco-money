8d8ccc77e5a0aa9adf0a5ae5f8f753c62a70b7ca
hyvnsevng <yh189487@gmail.com>
2024-11-22 21:50:54 +0900

    feat: 로그인 시 새로고침 없이 내비게이션 바 변경하도록 수정, 로그아웃 기능 추가, 금융상품추천 페이지 로그인 상태에서만 이동할 수 있도록 수정(로그아웃 상태일 시 메시지 창 통해 로그인 할 것인지 묻고, 확인 버튼 클릭 시 로그인 페이지로 이동) (FE)

--- front/zzocco_money/src/views/HomeView.vue
+++ front/zzocco_money/src/views/HomeView.vue
@@ -7,7 +7,7 @@
           나도 몰랐던 내 금융 성향, <br />
           지금 확인해보세요!
         </h1>
-        <button class="mt-4 px-6 py-3 bg-black text-white rounded hover:bg-gray-800">
+        <button @click="handleClick" class="mt-4 px-6 py-3 bg-black text-white rounded hover:bg-gray-800">
           내 금융 성향 알아보러 가기
         </button>
       </div>
@@ -15,7 +15,24 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { useRouter, RouterLink } from 'vue-router'
+import { useAccountStore } from '@/stores/account';
+import { computed } from 'vue';
+
+const router = useRouter()
+const store = useAccountStore()
+const isLoggedIn = computed(() => store.isLogin)
+
+const handleClick = () => {
+  if (!isLoggedIn.value) {
+    if (confirm('로그인 후 이용 가능합니다. \n로그인 하시겠습니까?')) {
+      router.push('login/')
+    }
+  } else {
+    router.push('/recommend') // 로그인 상태라면 recommend 페이지로 이동
+  }
+}
 </script>
 
 <style scoped>
