a3510591d0211e65ba61ec65848ee00d7df914c4
hyvnsevng <yh189487@gmail.com>
2024-11-26 12:52:21 +0900

    fix: 문법 오류 수정 (BE)

--- back/zzocco_money/accounts/urls.py
+++ back/zzocco_money/accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('user/', views.user_info, name='user_info'),   # 유저 정보 조회 및 수정
     path('', include('dj_rest_auth.urls')),  # dj_rest_auth 기본 URL
     path('manage-product/', views.manage_product, name='manage_product'),
-    path("upload-csv/", views.upload_csv)
+    path("upload-csv/", views.upload_csv),
     path('update-profile-image/', views.update_profile_image, name='update_profile_image')
 ]
